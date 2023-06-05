c5c56248ed1895af933d5e3387335f65189dffa0
Peru38 <mihail90tyapkin@gmail.com>
2023-06-05 16:41:47 +0800

    1/10

---
+++ header.js
@@ -0,0 +1,11 @@
+document.getElementById("menu").innerHTML='\
+    <nav>
+        <ul class="menu_inner">
+            <li class="menu_item"><a href="#">Форум</a></li>\
+            <li class="menu_item"><a href="#">Новости</a></li>\
+            <li class="menu_item"><a href="#">Библиотека</a></li>\
+            <li class="menu_item"><a href="./klan.html">Кланы</a></li>\
+            <li class="menu_item"><a href="./regist.html">Регистрация</a></li>\
+            <li class="menu_item"><a href="./auth.html">Вход</a></li>\
+        </ul>
+    </nav>
